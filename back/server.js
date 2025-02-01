b256549a4e962a8e0e967c599c30f1487b9411c1
guirossbach <guilhermerossbach12@gmail.com>
2025-02-01 14:18:09 -0300

    melhorias no arquivo server.js

--- back/server.js
+++ back/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
